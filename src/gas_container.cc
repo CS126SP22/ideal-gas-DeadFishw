9e1f3a1ed951b21224f543a10b79d595d43f4879
DeadFishw <46125791+DeadFishw@users.noreply.github.com>
2022-03-09 15:26:15 -0600

    Implement container

--- src/gas_container.cc
+++ src/gas_container.cc
@@ -1,23 +1,60 @@
 #include "gas_container.h"
+#include "gas_particle.h"
+
 
 namespace idealgas {
 
 using glm::vec2;
 
 GasContainer::GasContainer() {
-
+    for (int i = 0; i < kNumberOfParticle; i++) {
+        particles_.emplace_back(float(rand())/float((RAND_MAX)) * kLength, float(rand())/float((RAND_MAX)) * kWidth);
+    }
+    needAccelerate = false;
 }
 
 void GasContainer::Display() const {
   // This function has a lot of magic numbers; be sure to design your code in a way that avoids this.
   ci::gl::color(ci::Color("orange"));
-  ci::gl::drawSolidCircle(vec2(dummy_variable_, 200), 10);
-  ci::gl::color(ci::Color("white"));
-  ci::gl::drawStrokedRect(ci::Rectf(vec2(100, 100), vec2(600, 400)));
+  ci::gl::drawStrokedRect(ci::Rectf(vec2(0, 0), vec2(600, 400)));
+
+  for (const GasParticle particle: particles_) {
+      ci::gl::color(ci::Color("red"));
+      ci::gl::drawSolidCircle(particle.GetPosition(), particle.GetSize());
+  }
+
 }
 
 void GasContainer::AdvanceOneFrame() {
-  ++dummy_variable_;
+    for (GasParticle& particle: particles_) {
+        particle.update(kLength, kWidth);
+        if (needAccelerate) {
+            particle.update(kLength, kWidth);
+        }
+    }
+
+    for (size_t indexOfParticles = 0; indexOfParticles < particles_.size(); indexOfParticles++) {
+        for (size_t currentIndex = indexOfParticles + 1; currentIndex < particles_.size(); currentIndex++) {
+            GasParticle &particle = particles_.at(indexOfParticles);
+            particles_.at(currentIndex).CollideWithParticle(particle);
+        }
+    }
 }
+
+    void GasContainer::RemoveParticles() {
+        particles_.clear();
+    }
+
+    void GasContainer::AddParticle(GasParticle particle) {
+        particles_.push_back(particle);
+    }
+
+    std::vector<GasParticle> GasContainer::GetParticles() {
+        return particles_;
+    }
+
+    void GasContainer::Accelerate() {
+        needAccelerate = !needAccelerate;
+    }
 
 }  // namespace idealgas
